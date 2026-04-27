28b0595bf4e72c71653876fbe395778ad2f374ee
PhoenixJin23 <g3472863433@gmail.com>
2026-04-27 09:54:50 +0800

    文件分类器1.0落地！

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from tool.image_tool import run_image_tool
 from tool.pdf_tool import run_pdf_tool
+from tool.classify import run_classify
 
 
 
@@ -8,7 +9,7 @@ from tool.pdf_tool import run_pdf_tool
 st.sidebar.title("🛠️ Python 万能工具箱")
 menu = st.sidebar.radio(
     "请选择一个工具：",
-    ("首页介绍", "图片处理助手", "PDF合并工具", "账单分析仪")
+    ("首页介绍", "图片处理助手", "PDF合并工具", "文件分类器","账单分析仪")
 )
 
 # 2. 根据菜单选择，显示不同的内容
@@ -23,9 +24,12 @@ elif menu == "图片处理助手":
 elif menu == "PDF合并工具":
     run_pdf_tool()
 
+elif menu == "文件分类器":
+    run_classify()
+
 elif menu == "账单分析仪":
     st.title("💰 账单分析仪")
-    st.info("这里将集成了账单分析逻辑...")
+    st.info("这里集成了账单分析逻辑...")
 
 
 
---
+++ tool/classify.py
@@ -0,0 +1,88 @@
+import os
+import streamlit as st
+
+#task:扫描一个文件夹，根据文件的后缀名（如.jpg,.pdf,.py），自动把它们移动到对应的子文件夹里。
+
+def classify_files(target_dir):
+    #定义分类规则
+    extension_rules={
+        ".png":"images",
+        ".jpg":"images",
+        ".jpeg":"images",
+        ".heic":"images",
+        ".pdf":"documents",
+        ".docx":"documents",
+        ".txt":"documents",
+        ".py":"python_scripts",
+        ".zip":"archives"
+    }
+
+    #从目标文件夹中获取所有文件
+    files=[f for f in os.listdir(target_dir) if os.path.isfile(os.path.join(target_dir,f))]
+    if not files:
+        return "文件夹是空的，或者没有可分类的文件。"
+
+    count=0
+    log_placeholder=st.empty() #在网页留一块空间放日志
+    log_text="" #日志内容
+
+    for filename in files:
+        #构造文件的完整路径（文件夹+文件名）
+        old_path=os.path.join(target_dir,filename)
+        # 获取文件后缀名：os.path.splitext
+        _, extension = os.path.splitext(filename)
+        extension = extension.lower()  # 后缀名全部转化为小写
+        moved=False
+
+
+        for folder_name,extension in extension_rules.items():
+            if extension in extension_rules:
+                folder_name=extension_rules[extension] #由后缀名对应文件类型
+            else: #不在规则里，就去others
+                folder_name="others"
+
+            folder_path=os.path.join(target_dir,folder_name)
+
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+
+            #移动文件进行重新分类
+            new_path=os.path.join(folder_path,filename)
+
+            if not os.path.exists(new_path):
+                os.rename(old_path,new_path)
+                moved=True
+                log_text+=f"移动成功！{filename}---->{folder_name}"
+                count+=1
+            break
+        if not moved: #如果目标位置已经有同名文件了，先不移动，防止覆盖
+            log_text+=f"跳过未知格式：{filename}"
+    return f"分类完成！共处理了{count}个文件。"
+
+
+def run_classify():
+    st.header("📁 文件分类助手")
+    st.info("提示：本工具将扫描目标文件夹，根据文件后缀进行分类")
+
+    #用户输入
+    target_dir=st.text_input("目标文件夹","请输入待分类的文件夹地址")
+
+    #开始执行
+    if st.button("开始分类","primary"):
+        if not os.path.exists(target_dir):
+            st.error("错误！请检查输入的地址！")
+        else:
+            with st.spinner("正在分类，请稍后..."):
+                result=classify_files(target_dir)
+                st.success(result)
+                st.balloons()
+
+
+
+
+
+
+
+
+
+
